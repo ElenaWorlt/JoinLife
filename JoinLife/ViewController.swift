7f2fbfcafa6e75e106a3b22fa8b82ea1c6fc5217
Елена Душакова <elenad@MacBook-Air-Elena.local>
2022-12-16 17:28:45 +0500

    add code in app

--- JoinLife/ViewController.swift
+++ JoinLife/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultButton: UILabel!
+    private var numberOfDays = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        resultButton.text = "Узнай сколько дней ты уже на планете"
         // Do any additional setup after loading the view.
     }
 
-
+    @available(iOS 15, *)
+    @IBAction func passedButton(_ sender: UIDatePicker) {
+        let range = sender.date..<Date.now
+            let loc = Locale(identifier: "ru")
+        numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+    }
+    
+    @IBAction func tuppedButtonResult() {
+        resultButton.text = "Ты наслаждаешься жизнью уже \(numberOfDays)"
+    }
 }
 
